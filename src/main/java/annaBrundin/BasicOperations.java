dcd52db1a6e282629fc3508afefdb116da1fa2d8
ActionArmadillo <62772766+ActionArmadillo@users.noreply.github.com>
2021-04-01 16:02:08 +0200

    sorted out maven and intellij

--- src/main/java/annaBrundin/BasicOperations.java
+++
@@ -1,8 +0,0 @@
-package annaBrundin;
-
-public interface BasicOperations {
-    double addition(double value1, double value2);
-    double subtraction(double value1, double value2);
-    double multiplication(double value1, double value2);
-    double division(double value1, double value2);
-}
